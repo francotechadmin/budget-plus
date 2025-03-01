25aeab2f3cc2b2c12f6bc9e67d22524b29708773
Gabriel Franco <gabriel.a.franco@invista.com>
2025-03-01 16:26:09 -0600

    Add useAddTransactionMutation hook for adding transactions via API

---
+++ client/src/hooks/api/useAddTransactionMutation/index.ts
@@ -0,0 +1,60 @@
+// src/hooks/api/useAddTransactionMutation/index.ts
+
+import axios from "axios";
+import {
+  useMutation,
+  UseMutationResult,
+  useQueryClient,
+} from "@tanstack/react-query";
+
+import { Transaction } from "@/models/transactions";
+
+interface AddTransactionResponse {
+  transaction: Transaction;
+}
+
+interface AddTransactionMutationVariables {
+  date: string;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+const addTransactionFn = async ({
+  date,
+  description,
+  amount,
+  category,
+}: AddTransactionMutationVariables): Promise<AddTransactionResponse> => {
+  const response = await axios.post<AddTransactionResponse>(`/transactions/`, {
+    date: date,
+    description: description,
+    amount: amount,
+    category: category,
+  });
+  return response.data;
+};
+
+export const useAddTransactionMutation = (): UseMutationResult<
+  AddTransactionResponse,
+  Error,
+  AddTransactionMutationVariables
+> => {
+  const queryClient = useQueryClient();
+  return useMutation<
+    AddTransactionResponse,
+    Error,
+    AddTransactionMutationVariables
+  >({
+    mutationFn: addTransactionFn,
+    onSuccess: (data: AddTransactionResponse) => {
+      console.log("Transaction added successfully:", data);
+
+      // Invalidate and refetch the transactions query to update the UI
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+    },
+    onError: (error) => {
+      console.error("Error adding transaction:", error);
+    },
+  });
+};
