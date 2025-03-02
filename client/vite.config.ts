84f01d75ece955350aefdfeea1803ccb0fe27b3f
Gabriel Franco <gabriel.a.franco@invista.com>
2025-03-02 09:55:03 -0600

    Refactor Vite configuration to load environment variables dynamically and set base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,25 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }: { mode: string }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
+  console.log("VITE_BASE_PATH", process.env.VITE_BASE_PATH);
+  return defineConfig({
+    plugins: [TanStackRouterVite(), viteReact()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    host: true,
-    port: 3000,
-  },
-  base: "/budget-plus/",
-});
+    server: {
+      host: true,
+      port: 3000,
+    },
+    base: process.env.VITE_BASE_PATH || "/",
+  });
+};
