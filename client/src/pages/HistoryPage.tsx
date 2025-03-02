197dc872a0f9a96492ca7b2446f609620c68e254
Gabriel Franco <gabriel.a.franco@invista.com>
2025-03-01 22:26:05 -0600

    Add Skeleton loading state to HistoryPage for improved user experience during data fetching

--- client/src/pages/HistoryPage.tsx
+++ client/src/pages/HistoryPage.tsx
@@ -1,16 +1,24 @@
 import BarChartVertical from "../components/BarChartVertical"; // Import the BarChartVertical component
 import { useGetTransactionHistoryQuery } from "@/hooks/api/useGetTransactionHistoryQuery";
-
+import { Skeleton } from "@/components/ui/skeleton";
 const HistoryPage = () => {
-  const { data: transactions, isLoading: transactionsLoading } =
-    useGetTransactionHistoryQuery();
+  const {
+    data: transactions = {},
+    isLoading: transactionsLoading,
+    error,
+  } = useGetTransactionHistoryQuery();
 
-  if (transactionsLoading) {
-    return <div>Loading...</div>;
+  if (error) {
+    return <div>Error fetching data.</div>;
   }
 
-  if (!transactions) {
-    return <div>Error fetching data.</div>;
+  if (transactionsLoading) {
+    return (
+      <div className="container mx-auto px-4 mt-4">
+        <h1 className="text-2xl font-bold">History</h1>
+        <Skeleton className="h-[350px] w-full mt-4" />
+      </div>
+    );
   }
 
   return (
