f36323d65af4fff0c39e834a2793b50412b92e15
Gabriel Franco <gabriel.a.franco@invista.com>
2025-03-04 08:38:30 -0600

    Refactor transactions endpoints and remove Elasticsearch service from Docker Compose

--- api/app/main.py
+++ api/app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .database.database import engine, Base
 from .models.models import Base
-from .endpoints import ping, categories, transactions, elastic, users
+from .endpoints import ping, categories, users, transactions_crud, transactions_reporting, transactions_import
 
 app = FastAPI(title="Production-Ready API")
 
@@ -19,8 +19,9 @@ app.add_middleware(
 # Include routers with optional prefixes and tags
 app.include_router(ping.router)
 app.include_router(categories.router, prefix="/categories", tags=["Categories"])
-app.include_router(transactions.router, prefix="/transactions", tags=["Transactions"])
-app.include_router(elastic.router, prefix="/elastic", tags=["ElasticSearch"])
+app.include_router(transactions_crud.router, prefix="/transactions")
+app.include_router(transactions_reporting.router, prefix="/transactions")
+app.include_router(transactions_import.router, prefix="/transactions")
 app.include_router(users.router, prefix="/users", tags=["Users"])
 
 logging.basicConfig(level=logging.DEBUG)
